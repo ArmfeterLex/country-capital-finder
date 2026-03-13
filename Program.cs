761cd455bcda1ed8aadffca7cd9d404bff2d3035
ArmfeterLex <alexromanenkobeats@yandex.ru>
2026-03-13 20:34:36 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string mes;
+Console.WriteLine("Введите название страны: б, р, л");
+string country = Console.ReadLine();
+switch (country)
+{
+    case "б": mes = "Беларусь, Минск"; break;
+    case "р": mes = "Россия, Москва"; break;
+    case "л": mes = "Литва, Вильнюс"; break;
+    default: mes = "Некорректная буква страны"; break;
+}
+Console.WriteLine(mes);
+Console.ReadKey();
